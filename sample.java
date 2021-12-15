e3664544c2e2bee23b89635d504627fb2040780f
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-12-15 11:55:52 +0530

    added

--- sample.java
+++ sample.java
@@ -2,15 +2,24 @@ public class sample {
 
     public static void main(String[] args) {
 
-        Happy H1 = new Happy();
+        Happy H1 = new Happy(10,20);
 
     }
 
 }
 
 class Happy {
-
-    Happy() {
-        System.out.println("hello india");
+int a,b;
+    Happy(int a,int b) {
+    this.a=a;
+    this.b=b;
+       System.out.println(a+b);
+       styke();
     }
+    void styke(){
+        System.out.println(a+b);
+    }
+
+    
+    
 }
