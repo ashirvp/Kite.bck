f7fd81e3601c259439ddbfe8008505ccd4e8efb6
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-12-15 10:08:08 +0530

    added the code

--- back.java
+++ back.java
@@ -1,17 +1,21 @@
 public class back {
-    public static int[] twonumSum(int[] array,int target){
-        for(int i=0;i<array.length-1;i++){
-            for(int j=i+1;i<array.length;j++){
-                if(array[i]+array[j]==target){
-                    return new int[] {array[i],array[j]}
+    public static int[] twonumSum(int[] array, int target) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; i < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    return new int[] { array[i], array[j] };
                 }
             }
         }
         return new int[0];
     }
-}
-public static void main(String[] args) {
-    int[] array={6,5,7,9,4,0,2,1};
-    
 
+    public static void main(String[] args) {
+        int[] array = { 6, 5, 7, 9, 4, 0, 2, 1 };
+        int target = 10;
+        int[] result = twonumSum(array, target);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
 }
